a4a099b24161d46fb86ac1f0a6184cd39169813c
sanderdechering <sander.dechering@gmail.com>
2023-02-17 11:50:08 +0100

    Added a toggle component, which can toggle between a x axis of 0rem 5rem and 10rem. Used for switching between calculator themes.

---
+++ src/components/Toggle.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="relative h-6 w-full cursor-pointer rounded-full bg-My-very-dark-violet-3 px-1 pt-1.5 group-hover:after:translate-x-1"
+    @click="changeTheme"
+  >
+    <div
+      class="absolute h-3 w-3 rounded-full bg-My-soft-cyan duration-300 ease-in-out"
+      :class="'translate-x-' + bulletX.value"
+    ></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive } from "vue";
+
+const props = defineProps<{
+  toggle: object;
+}>();
+
+let theme = 2;
+let bulletX = reactive({
+  value: 10,
+});
+
+function changeTheme() {
+  if (bulletX.value === 10 && theme === 2) {
+    theme = 0;
+    bulletX.value = 0;
+    return;
+  }
+  theme += 1;
+  bulletX.value += 5;
+}
+</script>
+
+<style scoped></style>
